5e12fdcd4f3e4e1ae1ab65f54ef621d738876659
alexcasar <alexcasar@gmail.com>
2015-03-11 16:48:29 -0600

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -39,7 +39,7 @@ reserved = {
 
 tokens = [	'COMMA', 'SEMICOLON', 'COLON', 'PLUS', 'MINUS', 'MUL', 'DIV', 'LBRACE',
 		'RBRACE', 'LPAREN', 'RPAREN', 'CEQ', 'CNE', 'CLT', 'CGT', 'CLE', 'CGE',
-		'STRING', 'ID', 'FLOAT', 'INTEGER' ] + list(reserved.values())
+		'STRING', 'ID', 'FLOAT', 'INTEGER', 'EQU', 'AND', 'OR' ] + list(reserved.values())
 		
 t_ignore = ' \t\r'
 t_COMMA = r'\,'
