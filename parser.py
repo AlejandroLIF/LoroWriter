126d5d551c00c9de1a01e1fb6878f28242fa76f7
BlackRage83 <alejandrolealislaflores@hotmail.com>
2015-03-31 12:23:29 -0600

    Fixed bugs on parser.py, added instructions and colors, and renamed elements.
    Elements renamed to improve readability are as follows:
        functions to functionDecs
        function to functionDec
        assign_or_funccall to assignOrFunccall
        funccall to functionCall
        colorconstant to colorConstant
    
    Fixed error in functionCall: changed "params" to "args".
    Fixed error in "assignment", "condition", "loop" and "print", where the semicolon ";" was referenced twice.
    
    Added instructions format to draw "circle", "square" and "arc".
    Added more colors to colorConstant in order to reflect the changes in lexer.py.
    
    See documentation for the graphical representation of the syntax diagrams.

--- parser.py
+++ parser.py
@@ -9,169 +9,200 @@ precedence = (
 start = 'program'
 
 def p_program(p):
-	'''program	: PROGRAM ID program2'''
-	
-def p_program2(p):
-	'''program2	: vars function block END
-			| empty	'''
+	'''program	: PROGRAM ID vars functionDecs block end'''
 
-def p_function(p):
-	'''function	: FUNCTION ID LPAREN parameters RPAREN vars block END
-			| empty'''
 def p_vars(p):
-	'''vars		: VAR id1 
-			| empty '''
-def p_id1(p): 	
-	'''id1		: ID id2 COLON type SEMICOLON id3'''
-def p_id2(p):	
-	'''id2		: COMMA ID id2
-			| '''
-def p_id3(p):	
-	'''id3		: id1
-			| '''
-def p_type(p):
-	'''type		: INTEGER
-			| FLOAT
-			| STRING'''
+	'''vars		: VAR var 
+                | empty '''
 
+def p_var(p):
+    '''var      : ids COLON type SEMICOLON more_var'''
+    
+def p_more_var(p):
+    '''more_var : var
+                | empty'''
+
+def p_functionDecs(p):
+    '''functionDecs : functionDec functionDecs
+                    | empty'''
+
+def p_functionDec(p):
+	'''functionDec	: FUNCTION ID LPAREN params RPAREN vars block END'''
+
+def p_params(p):
+    '''params   : param
+                | empty'''
+
+def p_param(p):
+    '''param    : type ID more_param'''
+    
+def p_more_param(p):
+    '''more_param   : COMMA param
+                    | empty'''
+			
+def p_ids(p):
+    '''ids      : ID more_ids '''
+    
+def p_more_ids(p):
+    '''more_ids : COMMA ids
+                | empty'''
+			
+def p_type(p):
+	'''type     : INTEGER
+	            | FLOAT
+	            | STRING'''
+	            
 def p_block(p):
 	'''block	: LBRACE statements RBRACE'''
 	
 def p_statements(p):
-	'''statements	: empty
-			| statement statements'''
+	'''statements	: statement SEMICOLON statements
+                    | empty'''
 	
 def p_statement(p):
-	'''statement	: ID paraass
-			| instruction
-			| condition
-			| loop
-			| print'''
+	'''statement	: ID assignOrFunccall
+                    | instruction
+                    | condition
+                    | loop
+                    | print'''
 			
-def p_paraass(p):
-	'''paraass	: LPAREN args RPAREN
-			| assignment'''
-			
-def p_args(p):
-	'''args		: number args2'''
-	
-def p_args2(p):
-	'''args2	: COMMA args
-			| empty'''
-			
+def p_assignOrFunccall(p):
+    '''assignOrFunccall : assignment
+                        | functionCall'''
+                        
 def p_assignment(p):
-	'''assignment	: EQU expression SEMICOLON'''
+	'''assignment	: EQU expression'''
+			
+def p_functionCall(p):
+    '''functionCall : LPAREN args RPAREN'''
+
+def p_args(p):
+    '''args         : arg
+                    | empty'''
+
+def p_arg(p):
+    '''arg          : number more_arg'''
+                    
+def p_more_arg(p):
+    '''more_arg     : COMMA arg
+                    | empty'''
 	
-def p_parameters(p):
-	'''parameters	: type ID parameters2'''
-	
-def p_parameters2(p):
-	'''parameters2	: COMMA parameters
-			| empty'''
-def p_condition(p):
-	'''condition	: IF LPAREN expression RPAREN block cond2 SEMICOLON'''
-	
-def p_cond2(p):
-	'''cond2	: ELSE block
-			| empty '''
 def p_instruction(p):
 	'''instruction	: movement number
-			| DRAW boolean
-			| PRESSURE integer
-			| COLOR colorconstant'''
+                    | DRAW boolean
+                    | PRESSURE integer
+                    | COLOR colorConstant
+                    | ARC integer integer
+                    | CIRCLE integer
+                    | SQUARE integer integer'''
+			
 def p_movement(p):
 	'''movement	: FORWARD
-			| BACKWARD
-			| RIGHT
-			| LEFT'''
+                | BACKWARD
+                | RIGHT
+                | LEFT'''
+                
 def p_number(p):
 	'''number	: integer
-			| FLOAT'''
+                | FLOAT'''
 			
 def p_boolean(p):
 	'''boolean	: TRUE
-			| FALSE'''
+                | FALSE'''
 
 def p_integer(p):
 	'''integer	: ID
-			| INTEGER'''
+                | INTEGER'''
 			
-def p_colorconstant(p):
-	'''colorconstant: RED
-			| GREEN
-			| BLUE
-			| BLACK'''
-
+def p_colorConstant(p):
+	'''colorConstant    : RED
+	                    | ORANGE
+	                    | YELLOW
+                        | GREEN
+                        | BLUE
+                        | CYAN
+                        | PURPLE
+                        | WHITE
+                        | BLACK'''
+                        
+def p_condition(p):
+	'''condition	: IF LPAREN expression RPAREN block else'''
+	
+def p_else(p):
+	'''else	    : ELSE block
+			    | empty '''
+			
 def p_loop(p):
-	'''loop		: loophead block END SEMICOLON'''
+	'''loop     : loophead block END'''
 	
 def p_loophead(p):
 	'''loophead	: FOR LPAREN assignments SEMICOLON expression SEMICOLON ASSIGNMENTS RPAREN
-			| WHILE LPAREN expression RPAREN'''
+                | WHILE LPAREN expression RPAREN'''
 			
 def p_assignments(p):
-	'''assignments	: assignment assignments2'''
+	'''assignments	: ID assignment more_assignments'''
 	
-def p_assignments2(p):
-	'''assignments2 : COMMA assignments
-			| empty'''
+def p_more_assignments(p):
+	'''more_assignments : COMMA assignments
+                        | empty'''
 			
 def p_print(p):
-	'''print	: PRINT LPAREN printables RPAREN SEMICOLON'''
+	'''print	: PRINT LPAREN printables RPAREN'''
 	
 def p_printables(p):
-	'''printables	: printable printable2'''
+	'''printables	: printable more_printables'''
 	
-def p_printable2(p):
-	'''printable2	: COMMA printables
-			| empty'''
-			
+#Possible error detected: printing a "string variable"?
 def p_printable(p):
-	'''printable	: expression
-			| STRING'''
-			
+	'''printable    : expression
+                    | STRING'''
+                    
+def p_more_printables(p):
+	'''more_printables	: COMMA printables
+                        | empty'''
+
 def p_expression(p):
-	'''expression	: exp expression2'''
+	'''expression	: exp compareto'''
 	
-def p_expression2(p):
-	'''expression2	: comparator exp
-			| empty'''
+def p_compareto(p):
+	'''compareto	: comparator exp
+                    | empty'''
 			
 def p_comparator(p):
 	'''comparator	: CEQ
-			| CGT 
-			| CGE
-			| CLT
-			| CLE
-			| CNE
-			| AND
-			| OR'''
+                    | CGT 
+                    | CGE
+                    | CLT
+                    | CLE
+                    | CNE
+                    | AND
+                    | OR'''
 			
 def p_exp(p):
 	'''exp		: term exp2'''
 	
 def p_exp2(p):
 	'''exp2		: sumsub exp
-			| empty'''
+                | empty'''
 			
 def p_sumsub(p):
 	'''sumsub	: PLUS
-			| MINUS'''
+                | MINUS'''
 			
 def p_term(p):
 	'''term		: factor term2'''
 	
 def p_term2(p):
-	'''term2	: muldiv term'''
-	
+	'''term2	: muldiv term
+	            | empty'''
+
 def p_muldiv(p):
 	'''muldiv	: MUL
-			| DIV'''
+                | DIV'''
 			
 def p_factor(p):
 	'''factor	: LPAREN expression RPAREN
-			| number
-			| sumsub number'''
+                | number'''
 			
 def p_empty(p):
 	'empty :'
