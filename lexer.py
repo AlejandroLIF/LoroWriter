c960930c35743124171668d7f275dc36a8dbc465
BlackRage83 <alejandrolealislaflores@hotmail.com>
2015-04-03 18:12:34 -0600

    Removed escape characters from COMMA, SEMICOLON, COLON, DIV, LBRACE and RBRACE.     Fixed ID definition: IDs MAY NOT start with a number.     Updated the error function. It now shows the line number where the error was found.

--- lexer.py
+++ lexer.py
@@ -52,15 +52,15 @@ tokens = [	'COMMA', 'SEMICOLON', 'COLON', 'PLUS', 'MINUS', 'MUL', 'DIV', 'LBRACE
 		'STRING', 'ID', 'FLOAT', 'INTEGER', 'EQU', 'AND', 'OR' ] + list(reserved.values())
 		
 t_ignore = ' \t\r'
-t_COMMA = r'\,'
-t_SEMICOLON = r'\;'
-t_COLON = r'\:'
+t_COMMA = r','
+t_SEMICOLON = r';'
+t_COLON = r':'
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_MUL = r'\*'
-t_DIV = r'\/'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
+t_DIV = r'/'
+t_LBRACE = r'{'
+t_RBRACE = r'}'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_EQU = r'\='
@@ -75,7 +75,7 @@ t_OR = r'\|\|'
 t_STRING = r'\"[a-zA-z0-9 ]*\"'
 
 def t_ID(t):
-	r'[a-zA-Z0-9_]+'
+	r'[a-zA-Z_][a-zA-Z0-9_]*'
 	t.value = str(t.value)
 	t.type = reserved.get(t.value, 'ID')
 	return t
@@ -96,7 +96,7 @@ def t_newline(t):
 	t.lexer.lineno += len(t.value)
 
 def t_error(t):
-	print "Illegal character '%s'" % t.value[0]
+	print "Illegal character '%s' at line '%i' " % (t.value[0], t.lexer.lineno)
 	t.lexer.skip(1)
 	
 lex.lex() #Build the lexer
