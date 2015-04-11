4c50b812fcafd05f82c417072e4fcc4635cc50ca
BlackRage83 <alejandrolealislaflores@hotmail.com>
2015-04-10 20:24:16 -0500

    Added syntax-guided compilation. Added most of the syntax-guided-compilation instructions that had been already designed to the date. Pending:     temporary result data type assignment     temporary result data type verification Due to the missing elements, the code has not yet been tested.

--- parser.py
+++ parser.py
@@ -1,6 +1,14 @@
 import sys
 import ply.yacc as yacc
 from lexer import tokens
+from procedureDirectory import procedureDirectory
+from quadrupleGenerator import quadrupleGenerator
+
+currentDirectory = procedureDirectory("global")
+instructions = quadrupleGenerator()
+
+variableStack = []
+seenType = None
 
 precedence =    (
                 ('left', 'AND', 'OR'),
@@ -18,7 +26,13 @@ def p_vars(p):
                 | empty '''
 
 def p_var(p):
-    '''var      : ids COLON type SEMICOLON more_var'''
+    '''var      : ids COLON type seen_type SEMICOLON more_var'''
+    
+def p_seen_type(p):
+    '''seen_type :'''
+    global currentDirectory
+    while variableStack:
+        currentDirectory.add_variable(variableStack.pop(), seenType)
 
 def p_more_var(p):
     '''more_var : var
@@ -28,22 +42,41 @@ def p_functionDecs(p):
     '''functionDecs : functionDec functionDecs
                     | empty'''
 
+
 def p_functionDec(p):
-    '''functionDec  : FUNCTION ID LPAREN params RPAREN vars block END'''
+    '''functionDec  : FUNCTION ID seen_function_id LPAREN params RPAREN vars block END'''
+    global currentDirectory
+    currentDirectory = currentDirectory.parent
+    
+def p_seen_function_id(p):
+    '''seen_function_id :'''
+    global currentDirectory
+    currentDirectory.add_directory(p[-1])
+    currentDirectory = currentDirectory.get_directory(p[-1])
 
 def p_params(p):
     '''params   : param
                 | empty'''
 
 def p_param(p):
-    '''param    : type ID more_param'''
+    '''param    : type ID seen_param more_param'''    
+
+def p_seen_param(p):
+    '''seen_param   :'''
+    global currentDirectory
+    currentDirectory.add_variable(p[-1], seenType)
 
 def p_more_param(p):
     '''more_param   : COMMA param
                     | empty'''
 
 def p_ids(p):
-    '''ids      : ID more_ids '''
+    '''ids      : ID seen_ID more_ids '''
+
+def p_seen_ID(p):
+    '''seen_ID  :'''
+    global variableStack
+    variableStack.append(p[-1])
 
 def p_more_ids(p):
     '''more_ids : COMMA ids
@@ -53,6 +86,9 @@ def p_type(p):
     '''type     : INT
                 | FLO
                 | STR'''
+    global seenType
+    variableTypes = { 'int' : int, 'float' : float, 'string' : str }
+    seenType = variableTypes[p[1]]
 
 def p_block(p):
     '''block    : LBRACE statements RBRACE'''
@@ -62,11 +98,17 @@ def p_statements(p):
                     | empty'''
 
 def p_statement(p):
-    '''statement    : ID assignOrFunccall
+    '''statement    : ID seen_id_statement assignOrFunccall
                     | instruction
                     | condition
                     | loop
                     | print'''
+                    
+def p_seen_id_statement(p):
+    '''seen_id_statement    :'''
+    global curentDirectory, instructions
+    variable = currentDirectory.get_variable(p[-1])
+    instructions.pushOperand(variable)
 
 def p_assignOrFunccall(p):
     '''assignOrFunccall : assignment
@@ -74,7 +116,12 @@ def p_assignOrFunccall(p):
 
 #Warning: there is currently no way to assign a string value to a variable.
 def p_assignment(p):
-    '''assignment   : EQU ssuperexp'''
+    '''assignment   : EQU seen_EQU ssuperexp'''
+
+def p_seen_EQU(p):
+    '''seen_EQU :'''
+    global instructions
+    instructions.pushOperator(p[-1])
 
 def p_functionCall(p):
     '''functionCall : LPAREN args RPAREN'''
@@ -129,18 +176,88 @@ def p_colorConstant(p):
                         | BLACK'''
 
 def p_condition(p):
-    '''condition    : IF LPAREN ssuperexp RPAREN block else END'''
+    '''condition    : IF LPAREN ssuperexp RPAREN seen_condition block seen_condition_block else END'''
+    global instructions
+    pendingJump = instructions.popJumpStack()
+    instructions.setQuadrupleResult(pendingJump, instructions.nextInstruction)
+
+def p_seen_condition(p):
+    '''seen_condition   :'''
+    global instructions
+    condition = instructions.PopOperand()
+    #TEST IF type(condition) == bool
+    instructions.generateQuadruple("GOTOF", condition, 0, 0)
+    instructions.pushJumpStack(instructions.nextInstruction - 1)
+
+def p_seen_condition_block(p):
+    '''seen_condition_block :'''
+    global instructions
+    pendingJump = instructions.popJumpStack()
+    instructions.generateQuadruple("GOTO", 0, 0, 0)
+    instructions.pushJumpStack(instructions.nextInstruction -1)
+    instructions.setQuadrupleResult(pendingJump, instructions.nextInstruction)
 
 def p_else(p):
     '''else     : ELSE block
                 | empty '''
     
 def p_loop(p):
-    '''loop     : loophead block END'''
+    '''loop     : loophead block seen_loop_block END'''
+    global instructions
+    pendingJump = instructions.popJumpStack()
+    instructions.setQuadrupleResult(pendingJump, instructions.nextInstruction)
+    
+    
+def p_seen_loop_block(p):
+    '''seen_loop_block  :'''
+    global instructions
+    pendingJump = instructions.popJumpStack()
+    instructions.generateQuadruple("GOTO", 0, 0, pendingJump)
 
 def p_loophead(p):
-    '''loophead : FOR LPAREN assignments SEMICOLON ssuperexp SEMICOLON assignments RPAREN
-                | WHILE LPAREN ssuperexp RPAREN'''
+    '''loophead : FOR LPAREN assignments seen_assignments1 SEMICOLON ssuperexp seen_for_ssuperexp SEMICOLON assignments seen_assignments2 RPAREN
+                | WHILE LPAREN seen_while_LPAREN ssuperexp seen_while_ssuperexp RPAREN'''
+
+def p_seen_assignments1(p):
+    '''seen_assignments1    :'''
+    global instructions
+    instructions.pushJumpStack(instructions.nextInstruction)
+
+def p_seen_for_ssuperexp(p):
+    '''seen_for_ssuperexp(p):'''
+    global instructions
+    condition = instructions.popOperandStack()
+    #TEST IF type(condition) == bool
+    pendingJump = instructions.popJumpStack()
+    instructions.generateQuadruple("GOTOF", condition, 0, 0)
+    instructions.pushJumpStack(instructions.nextInstruction - 1)
+    instructions.pushJumpStack(instructions.nextInstruction + 1)
+    instructions.generateQuadruple("GOTO", 0, 0, 0)
+    instructions.pushJumpStack(instructions.nextInstruction - 1)
+    instructions.pushJumpStack(pendingJump)
+
+def p_seen_assignments2(p):
+    '''seen_assignments2    :'''
+    global instructions
+    pendingJump = instructions.popJumpStack()
+    instructions.generateQuadruple("GOTO", 0, 0, pendingJump)
+    pendingJump = instructions.popJumpStack()
+    instructions.setQuadrupleResult(pendingJump, instructions.nextInstruction)
+
+def p_seen_while_LPAREN(p):
+    '''seen_while_LPAREN    :'''
+    global instructions
+    instructions.pushJumpStack(instructions.nextInstruction)
+
+def p_seen_while_ssuperexp(p):
+    '''seen_while_ssuperexp :'''
+    global instructions
+    condition = instructions.popOperandStack()
+    #TEST IF type(condition) == bool
+    pendingJump = instructions.popJumpStack()
+    instructions.generateQuadruple("GOTOF", 0, 0, 0)
+    instructions.pushJumpStack(instructions.nextInstruction - 1)
+    instructions.pushJumpStack(pendingJump)
 
 def p_assignments(p):
     '''assignments  : ID assignment more_assignments'''
@@ -174,13 +291,34 @@ def p_ssuperexp2(p):
 def p_andor(p):
     '''andor        : AND
                     | OR'''
+    global instructions
+    instructions.pushOperator(p[-1])
 
 def p_superexp(p):
     '''superexp   : exp compareto'''
-
+    global instructions
+    if instructions.topOperatorEquals('&&') or instructions.topOperatorEquals('||') :
+        op2 = instructions.popOperand()
+        op1 = instructions.popOperand()
+        operator = instructions.popOperator()
+        result = "TEMPORARY" #ADD TEMPORARY VARIABLE HERE
+        instructions.generateQuadruple(operator, op1, op2, result)
+        instructions.pushOperand(result)
+    
+    
 def p_compareto(p):
-    '''compareto    : comparator exp
+    '''compareto    : comparator exp seen_exp
                     | empty'''
+
+def p_seen_exp(p):
+    '''seen_exp :'''
+    global instructions
+    op2 = instructions.popOperand()
+    op1 = instructions.popOperand()
+    operator = instructions.popOperator()
+    result = "TEPMORARY" #ADD TEMPORARY VARIABLE HERE
+    instructions.generateQuadruple(operator, op1, op2, result)
+    instructions.pushOperand(result)
 
 def p_comparator(p):
     '''comparator   : CEQ
@@ -189,9 +327,22 @@ def p_comparator(p):
                     | CLT
                     | CLE
                     | CNE'''
+    global instructions
+    instructions.pushOperator(p[-1])
 
 def p_exp(p):
-    '''exp      : term exp2'''
+    '''exp      : term seen_term exp2'''
+
+def p_seen_term(p):
+    '''seen_term    :'''
+    global instructions
+    if instructions.topOperatorEquals('+') or instructions.topOperatorEquals('-'):
+        op2 = instructions.popOperand()
+        op1 = instructions.popOperand()
+        operator = instructions.popOperator()
+        result = "TEPMORARY" #ADD TEMPORARY VARIABLE HERE
+        instructions.generateQuadruple(operator, op1, op2, result)
+        instructions.pushOperand(result)
 
 def p_exp2(p):
     '''exp2     : sumsub exp
@@ -200,6 +351,8 @@ def p_exp2(p):
 def p_sumsub(p):
     '''sumsub   : PLUS
                 | MINUS'''
+    global instructions
+    instructions.pushOperator(p[1])
 
 def p_term(p):
     '''term     : factor term2'''
@@ -211,10 +364,35 @@ def p_term2(p):
 def p_muldiv(p):
     '''muldiv   : MUL
                 | DIV'''
+    global instructions
+    instructions.pushOperator(p[-1])
 
 def p_factor(p):
-    '''factor   : LPAREN ssuperexp RPAREN
-                | number'''
+    '''factor   : LPAREN seen_LPAREN ssuperexp RPAREN seen_RPAREN
+                | number seen_number'''
+    global instructions
+    if instructions.topOperatorEquals('*') or instructions.topOperatorEquals('/'):
+        op2 = instructions.popOperand()
+        op1 = instructions.popOperand()
+        operator = instructions.popOperator()
+        result = "TEPMORARY" #ADD TEMPORARY VARIABLE HERE
+        instructions.generateQuadruple(operator, op1, op2, result)
+        instructions.pushOperand(result)
+
+def p_seen_LPAREN(p):
+    '''seen_LPAREN  :'''
+    global instructions
+    instructions.pushOperand(p[-1])
+    
+def p_seen_RPAREN(p):
+    '''seen_RPAREN  :'''
+    global instructions
+    instructions.popOperand()
+
+def seen_number(p):
+    '''seen_number  :'''
+    global instructions
+    instructions.pushOperand(p[-1])
 
 def p_empty(p):
     'empty :'
