92ce0387949a5f5cd7a1c127ffdaaea88c0a8ab0
BlackRage83 <alejandrolealislaflores@hotmail.com>
2015-04-15 22:09:16 -0500

    replaced all tab characters with four spaces to better suit PEP 8 formatting style

--- lexer.py
+++ lexer.py
@@ -1,56 +1,56 @@
 import ply.lex as lex
 
 reserved = {
-	'program'	: 'PROGRAM',
-	'var'		: 'VAR',
-	'function'	: 'FUNCTION',
-	
-	'if'		: 'IF',
-	'else'		: 'ELSE',
-	
-	'while'		: 'WHILE',
-	'for'		: 'FOR',
-	
-	'end'		: 'END',
-	
-	'print'		: 'PRINT',
-	
-	'forward'	: 'FORWARD',
-	'backward'	: 'BACKWARD',
-	'right'		: 'RIGHT',
-	'left'		: 'LEFT',
-	
-	'draw'		: 'DRAW',
-	'arc'		: 'ARC',
-	'circle'	: 'CIRCLE',
-	'square'	: 'SQUARE',
-	
-	'pressure'	: 'PRESSURE',
-	'color'		: 'COLOR',
-	
-	'true'		: 'TRUE',
-	'false'		: 'FALSE',
-	
-	'red'		: 'RED',
-	'orange'	: 'ORANGE',
-	'yellow'	: 'YELLOW',
-	'green'		: 'GREEN',
-	'blue'		: 'BLUE',
-	'cyan'		: 'CYAN',
-	'purple'	: 'PURPLE',
-	'white'		: 'WHITE',
-	'black'		: 'BLACK',
-	
-	
-	'int'		: 'INT',
-	'float'		: 'FLO',
-	'string'	: 'STR'
+    'program'    : 'PROGRAM',
+    'var'        : 'VAR',
+    'function'    : 'FUNCTION',
+    
+    'if'        : 'IF',
+    'else'        : 'ELSE',
+    
+    'while'        : 'WHILE',
+    'for'        : 'FOR',
+    
+    'end'        : 'END',
+    
+    'print'        : 'PRINT',
+    
+    'forward'    : 'FORWARD',
+    'backward'    : 'BACKWARD',
+    'right'        : 'RIGHT',
+    'left'        : 'LEFT',
+    
+    'draw'        : 'DRAW',
+    'arc'        : 'ARC',
+    'circle'    : 'CIRCLE',
+    'square'    : 'SQUARE',
+    
+    'pressure'    : 'PRESSURE',
+    'color'        : 'COLOR',
+    
+    'true'        : 'TRUE',
+    'false'        : 'FALSE',
+    
+    'red'        : 'RED',
+    'orange'    : 'ORANGE',
+    'yellow'    : 'YELLOW',
+    'green'        : 'GREEN',
+    'blue'        : 'BLUE',
+    'cyan'        : 'CYAN',
+    'purple'    : 'PURPLE',
+    'white'        : 'WHITE',
+    'black'        : 'BLACK',
+    
+    
+    'int'        : 'INT',
+    'float'        : 'FLO',
+    'string'    : 'STR'
 }
 
-tokens = [	'COMMA', 'SEMICOLON', 'COLON', 'PLUS', 'MINUS', 'MUL', 'DIV', 'LBRACE',
-		'RBRACE', 'LPAREN', 'RPAREN', 'CEQ', 'CNE', 'CLT', 'CGT', 'CLE', 'CGE',
-		'STRING', 'ID', 'FLOAT', 'INTEGER', 'EQU', 'AND', 'OR' ] + list(reserved.values())
-		
+tokens = [    'COMMA', 'SEMICOLON', 'COLON', 'PLUS', 'MINUS', 'MUL', 'DIV', 'LBRACE',
+        'RBRACE', 'LPAREN', 'RPAREN', 'CEQ', 'CNE', 'CLT', 'CGT', 'CLE', 'CGE',
+        'STRING', 'ID', 'FLOAT', 'INTEGER', 'EQU', 'AND', 'OR' ] + list(reserved.values())
+        
 t_ignore = ' \t\r'
 t_COMMA = r','
 t_SEMICOLON = r';'
@@ -75,28 +75,28 @@ t_OR = r'\|\|'
 t_STRING = r'\"[a-zA-z0-9 ]*\"'
 
 def t_ID(t):
-	r'[a-zA-Z_][a-zA-Z0-9_]*'
-	t.value = str(t.value)
-	t.type = reserved.get(t.value, 'ID')
-	return t
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.value = str(t.value)
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
 
 def t_FLOAT(t):
-	r'[0-9]+\.[0-9]+'
-	t.value = float(t.value)
-	return t
+    r'[0-9]+\.[0-9]+'
+    t.value = float(t.value)
+    return t
 
 def t_INTEGER(t):
-	r'[0-9]+'
-	t.value = int(t.value)
-	return t
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
 
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 def t_error(t):
-	print "Illegal character '%s' at line '%i' " % (t.value[0], t.lexer.lineno)
-	t.lexer.skip(1)
-	
+    print "Illegal character '%s' at line '%i' " % (t.value[0], t.lexer.lineno)
+    t.lexer.skip(1)
+    
 lex.lex() #Build the lexer
